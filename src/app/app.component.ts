1e20878d3ec854ab0f87fcd5ebc3bb93d560e864
lorrainetechnology <lorrainetechnology@gmail.com>
2022-05-26 20:46:54 +0530

    angular basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  template: `<p>Hello World!</p>`,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'basics';
+
+export class AppComponent{
+  name = 'Lana'
+  imgUrl = 'https://picsum.photos/id/237/500/500'
+
+  getName() {
+    return this.name
+  }
+
+  changeImage(e: KeyboardEvent) {
+    this.imgUrl = (e.target as HTMLInputElement).value
+  }
+
+  logImg(event: string) {
+
+  }
 }
